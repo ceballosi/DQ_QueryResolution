71dbf78df4935bc1f6d3c496d48704dc8aa64ce9
Rick Rees <rick.rees@genomicsengland.co.uk>
2017-03-02 10:59:14 +0000

    Initial Filter screen layout

--- app/services/IssueTrackingService.scala
+++ app/services/IssueTrackingService.scala
@@ -1,10 +1,12 @@
 package services
 
-import java.util.Date
 import javax.inject.{Inject, Singleton}
 
-import dao.IssueTrackingDao
 import domain._
+import java.util.Date
+
+import dao.IssueTrackingDao
+import org.joda.time.LocalDateTime
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -17,7 +19,7 @@ class IssueTrackingServiceImpl @Inject()(issueTrackingDao: IssueTrackingDao)(imp
 
   def allIssues: Future[List[LoggedIssue]] = {
     Future {
-      List(LoggedIssue(
+      var issues = List(LoggedIssue(
         Open,
         "RYJ-Orp-031",
         "RJ",
@@ -37,7 +39,91 @@ class IssueTrackingServiceImpl @Inject()(issueTrackingDao: IssueTrackingDao)(imp
         None,
         None,
         Some("Comments on issue")
+      ), LoggedIssue(
+        Open,
+        "RYJ-Orp-032",
+        "RJ",
+        new LocalDateTime().minusDays(10).toDate,
+        DataQuality,
+        "RYJ",
+        None,
+        "110110124",
+        None,
+        Some("Group size"),
+        "Issue with family group size: Please see 'Group Issues' tab for more details and potential resolutions",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some("Comments on issue")
+      ), LoggedIssue(
+        Open,
+        "RYJ-Orp-033",
+        "RJ",
+        new LocalDateTime().minusDays(4).toDate,
+        DataQuality,
+        "RYJ",
+        None,
+        "110100125",
+        None,
+        Some("Group size"),
+        "Issue with family group size: Please see 'Group Issues' tab for more details and potential resolutions",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some("Comments on issue")
+      ), LoggedIssue(
+        Open,
+        "RYJ-Orp-034",
+        "RJ",
+        new LocalDateTime().minusDays(20).toDate,
+        DataQuality,
+        "RYJ",
+        None,
+        "110020126",
+        None,
+        Some("Group size"),
+        "Issue with family group size: Please see 'Group Issues' tab for more details and potential resolutions",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some("Comments on issue")
+      ), LoggedIssue(
+        Open,
+        "RYJ-Orp-035",
+        "RJ",
+        new LocalDateTime().minusDays(110).toDate,
+        DataQuality,
+        "RYJ",
+        None,
+        "110920127",
+        None,
+        Some("Group size"),
+        "Issue with family group size: Please see 'Group Issues' tab for more details and potential resolutions",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some("Comments on issue")
       ))
+      for(x <- 1 to 5){
+        issues = issues ::: issues
+      }
+      issues
     }
   }
 
